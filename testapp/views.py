359244c820efc092ba55eecb1affd4f67e1d11c3
polarcana <32035766+polarcana@users.noreply.github.com>
2021-01-14 23:36:53 -0800

    adding website to repository

---
+++ testapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+    # return HttpResponse('homepage')
+    return render(request, 'index.html') #ttpResponse('Hello WOrld!')
